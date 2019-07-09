565aa6f37be03a2661a4b2a3cd6988a89294cf59
eastend-street <ss_1322irf@icloud.com>
2019-07-09 16:57:15 -0700

    make 2d reactangle

--- src/Cell.java
+++ src/Cell.java
@@ -1,10 +1,20 @@
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
-public class Cell extends JPanel {
+public class Cell extends JPanel{
+    private int x = 0;
+    private int y = 0;
+    public int width = 20;
+    public int height = 20;
+    public Cell (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawLine(100,100,200,200);
+        g.drawRect(x,y,width,height);
     }
 }
 
--- src/GameArea.java
+++ src/GameArea.java
@@ -9,9 +9,35 @@ public class GameArea extends JFrame {
         this.setSize(500, 500);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ImageIcon icon= getImageIcon("https://images.discordapp.net/avatars/398127484983443468/0bc43684999726e69c2ca797200ffffc.png?size=512");
-        this.add(new Cell());
+        ImageIcon icon = getImageIcon("https://images.discordapp.net/avatars/398127484983443468/0bc43684999726e69c2ca797200ffffc.png?size=512");
+        drawArea();
         this.setVisible(true);
+    }
+
+    Cell[][] cells = new Cell[10][10];
+
+    void drawArea() {
+        int posX = 0;
+        int posY = 0;
+        Cell aCell = new Cell(0, 0);
+        for (int x = 0; x < cells.length; x++) {
+            for (int y = 0; y < cells[x].length; y++) {
+
+                aCell = new Cell(posX, posY);
+                this.add(aCell);
+                // cells[x][y] = aCell;
+                posY += aCell.width;
+                if (y >= cells[x].length - 1) {
+                    posY = 0;
+                }
+            }
+            posX += aCell.height;
+            if (posX == cells.length - 1) {
+                posX = 0;
+            }
+        }
+
+        this.add(new Cell(30, 30));
     }
 
     ImageIcon getImageIcon(String argUrl) {
