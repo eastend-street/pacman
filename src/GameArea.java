c78e6fda7353ffd23a2e4424ecb1f0abc5578a11
eastend-street <ss_1322irf@icloud.com>
2019-07-09 13:59:04 -0700

    add image

--- src/GameArea.java
+++ src/GameArea.java
@@ -1,7 +1,7 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.Container;
-import java.awt.BorderLayout;
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+import java.net.MalformedURLException;
 
 public class GameArea extends JFrame {
     public GameArea(String title, int width, int height) {
@@ -9,7 +9,29 @@ public class GameArea extends JFrame {
         setSize(width, height);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         JButton btn = new JButton("New button");
         add(btn, BorderLayout.NORTH);
+
+        ImageIcon icon= getImageIcon("https://images.discordapp.net/avatars/398127484983443468/0bc43684999726e69c2ca797200ffffc.png?size=512");
+        setIconImage(icon.getImage());
+
+        JLabel label1 = new JLabel(icon);
+
+        JPanel panel = new JPanel();
+        panel.add(label1);
+        add(panel, BorderLayout.CENTER);
+    }
+
+
+    ImageIcon getImageIcon(String argUrl) {
+        URL url = null;
+        try {
+            url = new URL(argUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        ImageIcon icon = new ImageIcon(url);
+        return icon;
     }
 }
